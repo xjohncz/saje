3fc65744178831122944aff4a07048a57aa93ff3
sje397 <sje397@200c22c1-0253-0410-a7cc-2773c7d12e9d>
2009-06-23 03:59:27 +0000

    listen to chat state events, sent messages, and clist double-clicks when detecting idle

--- trunk/plugins/autoaway/autoaway.h
+++ trunk/plugins/autoaway/autoaway.h
@@ -4,13 +4,16 @@
 #include <options_i.h>
 #include <accounts_i.h>
 #include <autoaway_i.h>
+#include <events_i.h>
+#include <message_window_i.h>
+#include <clist_i.h>
 #include "autoawayoptions.h"
 #include <QTimer>
 #include <QPoint>
 #include <QDateTime>
 #include <QMap>
 
-class AutoAway: public AutoAwayI {
+class AutoAway: public AutoAwayI, EventsI::EventListener {
 	Q_OBJECT
 
 public:
@@ -22,6 +25,8 @@ public:
 	bool pre_shutdown();
 	bool unload();
 	const PluginInfo &get_plugin_info();
+
+        bool event_fired(EventsI::Event &e);
 
 protected slots:
 	void options_applied();
